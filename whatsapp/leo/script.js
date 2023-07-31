cd45c241d3b3910528d2c56c0b554d5d3b9e734f
There Is No Earth B <53037110+thereisnoearthb@users.noreply.github.com>
2023-07-31 09:51:39 +0530

    Add files via upload

---
+++ whatsapp/leo/script.js
@@ -0,0 +1,343 @@
+// Data for each person with multiple phone numbers
+const peopleData = [
+  {
+    name: "Birendra Prasad Baishya",
+    constituency: "",
+    party: "Asom Gana Parishad",
+    phoneNumbers: ["919868181789"]
+  },
+  {
+    name: "Ajit Kumar",
+    constituency: "",
+    party: "Independent",
+    phoneNumbers: ["919868181223"]
+  },
+  {
+    name: "Rwngwra Narzary",
+    constituency: "",
+    party: "United People's Party(Liberal)",
+    phoneNumbers: ["919868181202"]
+  },
+  {
+    name: "Dhiraj Prasad Sahu",
+    constituency: "",
+    party: "Indian National Congress",
+    phoneNumbers: ["919934111111", "919717000234"]
+  },
+  {
+    name: "Prem Chand Gupta",
+    constituency: "",
+    party: "RJD",
+    phoneNumbers: ["919013181666"]
+  },
+  {
+    name: "Harivansh",
+    constituency: "",
+    party: "JDU",
+    phoneNumbers: ["919013181002", "919013181002"]
+  },
+  {
+    name: "Rajmani Patel",
+    constituency: "",
+    party: "INC",
+    phoneNumbers: ["919868181022", "919868181022"]
+  },
+  
+  {
+    name: "Rajani Ashokrao",
+    constituency: "",
+    party: "INC",
+    phoneNumbers: ["919422242222"]
+  },
+
+  {
+    name: "Mukul Balkrishna",
+    constituency: "",
+    party: "INC",
+    phoneNumbers: ["919811658584"]
+  },
+  {
+    name: "Sushil Kumar Modi",
+    constituency: "",
+    party: "BJP",
+    phoneNumbers: ["919431017530"]
+  },
+  {
+    name: "Sant Balbir Singh",
+    constituency: "",
+    party: "AAP",
+    phoneNumbers: ["919013181663", "919417319463"]
+  },
+ 
+  {
+    name: "R Dharmar",
+    constituency: "",
+    party: "AIADMK",
+    phoneNumbers: ["919443443661", "919443443661"]
+  },
+  {
+    name: "N. R. Elango",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919499933998"]
+  },
+  {
+    name: "S Kalyanasundaram",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919487691779", "919487691779"]
+  },
+  {
+    name: "Dr. Kanimozhi NVN Somu",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919499933994", "919499933994"]
+  },
+  {
+    name: "K.R.N. Rajeshkumar",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919442641618", "919442641618"]
+  },
+ 
+  {
+    name: "C.V. Shanmugam",
+    constituency: "",
+    party: "AIADMK",
+    phoneNumbers: ["919013181822", "919013181822"]
+  },
+  {
+    name: "M Shanmugham",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919821010395"]
+  },
+  {
+    name: "Tiruchi Siva",
+    constituency: "",
+    party: "DMK",
+    phoneNumbers: ["919443160180", "919443160180"]
+  },
+  {
+    name: "Vaiko",
+    constituency: "",
+    party: "MDMK",
+    phoneNumbers: ["919842161241", "919842161241"]
+  },
+ 
+
+  {
+    name: "P. Bhattacharya",
+    constituency: "",
+    party: "INC",
+    phoneNumbers: ["919433003922", "919433003922"]
+  },
+  {
+    name: "Bikash Ranjan Bhattacharya",
+    constituency: "",
+    party: "CPI(M)",
+    phoneNumbers: ["919013181922", "919013181922"]
+  },
+  {
+    name: "Abir Ranjan Biswas",
+    constituency: "",
+    party: "AITC",
+    phoneNumbers: ["919332274419", "919883293922"]
+  },
+  {
+    name: "Subhashish Chakraborty",
+    constituency: "",
+    party: "AITC",
+    phoneNumbers: ["919831159772"]
+  },
+  {
+    name: "Shanta Chhetri",
+    constituency: "",
+    party: "AITC",
+    phoneNumbers: ["917432036898"]
+  },
+ 
+  {
+    name: "Jawhar Sircar",
+    constituency: "",
+    party: "AITC",
+    phoneNumbers: ["919868181120"]
+  },
+  {
+    name: "Abhishek Manu Singhvi",
+    constituency: "",
+    party: "INC",
+    phoneNumbers: ["919810027705"]
+  },
+  {
+    name: "Dola Sen",
+    constituency: "",
+    party: "AITC",
+    phoneNumbers: ["919433155577", "919830361241"]
+  },
+ 
+  {
+    name: "Dr. Wanwieroy Kharlukhi",
+    constituency: "",
+    party: "National Peoples Party",
+    phoneNumbers: ["919485122202"]
+  },
+  {
+    name: "Abdul Wahab",
+    constituency: "",
+    party: "Indian Union Muslim League",
+    phoneNumbers: ["919013181677", "919868181280", "919847020911"]
+  },
+  {
+    name: "Elamaram Kareem",
+    constituency: "",
+    party: "Communist Party of India(Marxist)",
+    phoneNumbers: ["919868181068", "919400307786"]
+  },
+  {
+    name: "Jose. K Mani",
+    constituency: "",
+    party: "Kerala Congress(M)",
+    phoneNumbers: ["919447067772"]
+  },
+  {
+    name: "Vayalar Ravi",
+    constituency: "",
+    party: "Indian National Congress",
+    phoneNumbers: ["919910047474"]
+  },
+  {
+    name: "Dr L. Hanumanthaiah",
+    constituency: "",
+    party: "Indian National Congress",
+    phoneNumbers: ["919654087483"]
+  },
+  {
+    name: "Jairam Ramesh",
+    constituency: "",
+    party: "Indian National Congress",
+    phoneNumbers: ["919868181302"]
+  },
+  {
+    name: "K.T.S. Tulsi",
+    constituency: "",
+    party: "Indian National Congress",
+    phoneNumbers: ["919999609157"]
+  },{
+    name: "K.T.S. Tulsi",
+    party: "Indian National Congress",
+    phoneNumbers: ["919999609157"]
+    },
+    {
+    name: "Mohammed Nadihul Haque",
+    party: "AITC",
+    phoneNumbers: ["919013181969"]
+    },
+    {
+    name: "Sukhendu Sekhar Ray",
+    party: "AITC",
+    phoneNumbers: ["919013181188"]
+    },
+    {
+    name: "Santanu Sen",
+    party: "AITC",
+    phoneNumbers: ["919830144496"]
+    },
+    {
+    name: "Ranjan Gogoi",
+    party: "Nom.",
+    phoneNumbers: ["919435706522"]
+    },
+    {
+    name: "Santosh Kumar Joginipally",
+    party: "TRS",
+    phoneNumbers: ["917382222666"]
+    },
+    {
+    name: "K.Keshava Rao",
+    party: "TRS",
+    phoneNumbers: ["919966681039"]
+    },
+    {
+    name: "K.R Suresh Reddy",
+    party: "TRS",
+    phoneNumbers: ["919013181865"]
+    },
+    {
+    name: "P.T Usha",
+    party: "Nom.",
+    phoneNumbers: ["919846102345", "919846114939"]
+    },
+    {
+    name: "Gulam Ali",
+    party: "Nom.",
+    phoneNumbers: ["919419453000", "919419553000"]
+    },
+    {
+    name: "Ilayairaaja",
+    party: "Nom.",
+    phoneNumbers: ["919379274477"]
+    }
+];
+
+// Function to generate WhatsApp message
+function generateMessage(name, constituency, party, phoneNumber) {
+  var message = `Hon’ble MP ${name},
+I'm attaching a letter to members of the Rajya Sabha that has been endorsed by over 1000 citizens and organizations. 
+
+`;
+
+  var whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  window.open(whatsappLink, "_blank");
+}
+
+// Function to shuffle an array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Function to create a card element
+function createCard(person) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const img = document.createElement("img");
+  img.src = "https://prsindia.org/files/mptrack/profile_image/rajya-sabha/profile_image/900101.jpg";
+  img.width = "90%";
+  img.alt = "";
+  card.appendChild(img);
+
+  const h1 = document.createElement("h1");
+  h1.textContent = person.name;
+  card.appendChild(h1);
+
+  const p1 = document.createElement("p");
+  p1.innerHTML = `<b>Party:</b> ${person.party}`;
+  card.appendChild(p1);
+
+  // Loop through multiple phone numbers if available for a person
+  person.phoneNumbers.forEach(phoneNumber => {
+    const whatsappBtn = document.createElement("a");
+    whatsappBtn.classList.add("whatsapp-icon");
+    whatsappBtn.href = "#";
+    whatsappBtn.onclick = () => generateMessage(person.name, person.constituency, person.party, phoneNumber);
+    whatsappBtn.innerHTML = '<i class="fab fa-whatsapp"></i>';
+    card.appendChild(whatsappBtn);
+  });
+
+  return card;
+}
+
+// Shuffle the peopleData array
+shuffleArray(peopleData);
+
+// Create cards and append to the card container
+const cardContainer = document.getElementById("cardContainer");
+peopleData.forEach(person => {
+  const card = createCard(person);
+  cardContainer.appendChild(card);
+});
